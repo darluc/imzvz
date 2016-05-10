e2a665e19f97efde31b71790bad7c79f4892dccb
darluc <zwz252@gmail.com>
2016-05-10 19:44:46 +0800

    1.加入最简单的自动应答;

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,14 +2,24 @@
 
 namespace Furbook\Http\Controllers;
 
+use EasyWeChat\Server\Guard;
 use Illuminate\Http\Request;
 
 use Furbook\Http\Requests;
 
 class WechatController extends Controller
 {
-    public function connect(){
-        $response = app('wechat')->server->serve();
-        return $response;
+    public function connect(Request $request){
+        /** @var Guard $server */
+        $server = app('wechat')->server;
+
+        if(!$request->isMethod('get')) {
+            // only add message handler when the request is not using a get method
+            $server->setMessageHandler(function ($message) {
+                return "您好！欢迎关注我!";
+            });
+        }
+        
+        return $server->serve();
     }
 }
