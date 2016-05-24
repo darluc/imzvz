99756eb79bdeab6772a7d427e639a429370f64a3
darluc <zwz252@gmail.com>
2016-05-24 19:21:11 +0800

    1. add voice message support.

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,6 +2,8 @@
 
 namespace Furbook\Http\Controllers;
 
+use EasyWeChat\Message\Text;
+use EasyWeChat\Message\Voice;
 use EasyWeChat\Server\Guard;
 use Furbook\Services\TranslateService;
 use Illuminate\Http\Request;
@@ -18,26 +20,47 @@ class WechatController extends Controller
         if (!$request->isMethod('get')) {
             // only add message handler when the request is not using a get method
             $server->setMessageHandler(function ($message) {
-                if ($message->MsgType == 'text') {
-                    // do the translation
-                    /** @var TranslateService $tr */
-                    $tr = app('translateEngine');
-                    $content = $message->Content;
-                    // if it is Chinese, set target language to English
-                    if(preg_match("/[\\x{4e00}-\\x{9fa5}]/u",$content)){
-                        $tr->setTarget('en');
-                    }
-                    try {
-                        return $tr->translate($content);
-                    }catch (\Exception $ex){
-                        return 'Sorry, something goes wrong. Please wait a moment.';
-                    }
-                } else {
-                    return "您好！欢迎关注我!";
+                switch ($message->MsgType) {
+                    case 'text':
+                        return $this->textMessage($message);
+                    case 'voice':
+                        return $this->voiceMessage($message);
+                    default:
+                        return "更多功能, 敬请期待!!!";
                 }
             });
         }
 
         return $server->serve();
+    }
+
+    private function textMessage(Text $message)
+    {
+        $content = $message->Content;
+        return $this->translateIt($content);
+    }
+
+    private function voiceMessage(Voice $message)
+    {
+        if (!empty($message->Recognition)) {
+            return $this->translateIt($message->Recognition);
+        }else{
+            return 'ˋ(′o‵")ˊ';
+        }
+    }
+
+    private function translateIt($content)
+    {
+        /** @var TranslateService $tr */
+        $tr = app('translateEngine');
+        // if it is Chinese, set target language to English
+        if (preg_match("/[\\x{4e00}-\\x{9fa5}]/u", $content)) {
+            $tr->setTarget('en');
+        }
+        try {
+            return $tr->translate($content);
+        } catch (\Exception $ex) {
+            return 'Sorry, something goes wrong. Please wait a moment.';
+        }
     }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Furbook\Cat;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -40,5 +41,113 @@ class ExampleTest extends TestCase
 //        $result = $tr->setTarget('zh-CN')->translate('me');
 //        $this->assertEquals('我', $result);
     }
+
+    private function generateReport($runData, $namespace)
+    {
+        $xhprof_runs = new XHProfRuns_Default();
+        $run_id = $xhprof_runs->save_run($runData, $namespace);
+        echo "---------------\n" .
+            "XHProf: you can view run at \n" .
+            "http://xhprof.test/index.php?run=$run_id&source=$namespace\n" .
+            "---------------\n";
+    }
+
+    public function slimPdo()
+    {
+        $dsn = 'mysql:host=localhost;dbname=furbook;charset=utf8';
+        $usr = 'root';
+        $pwd = 'ddff999666';
+        $ahuaData = [
+            'name' => 'ahua',
+            'breed_id' => 3,
+            'user_id' => 1
+        ];
+        $keys = array_keys($ahuaData);
+        $vals = array_values($ahuaData);
+        xhprof_enable();
+        $pdo = new \Slim\PDO\Database($dsn, $usr, $pwd);
+
+        $selectStatement = $pdo->select(['name'])
+            ->from('cats');
+        $stmt = $selectStatement->execute();
+        $cats = $stmt->fetchAll();
+        var_dump($cats);
+        $this->assertEquals(1, count($cats));
+
+        $insertStatement = $pdo->insert($keys)
+            ->into('cats')
+            ->values($vals);
+        $insertStatement->execute(false);
+        
+        $selectStatement = $pdo->select(['name'])
+            ->from('cats');
+        $stmt = $selectStatement->execute();
+        $cats = $stmt->fetchAll();
+        $this->assertEquals(2, count($cats));
+
+        $deleteStatement = $pdo->delete()
+            ->from('cats')
+            ->where('name', '=', 'ahua');
+        $deleteStatement->execute();
+
+        for ($i = 0; $i < 10000; $i++) {
+            $selectStatement = $pdo->select(['name'])
+                ->from('cats');
+            $stmt = $selectStatement->execute();
+            $cats = $stmt->fetchAll();
+        }
+        $this->assertEquals(1, count($cats));
+        $xhprofData = xhprof_disable();
+        $this->generateReport($xhprofData, 'slim-pdo');
+    }
+
+    public function testDBPerformance()
+    {
+        include_once __DIR__ . "/xhprof_lib/utils/xhprof_lib.php";
+        include_once __DIR__ . "/xhprof_lib/utils/xhprof_runs.php";
+        $ahuaData = [
+            'name' => 'ahua',
+            'breed_id' => 3,
+            'user_id' => 1
+        ];
+        $this->slimPdo();
+
+        $lockfile = __DIR__ . '/.dblock';
+
+        if (file_exists($lockfile)) {
+            //------------------- Query builder ------------------
+            xhprof_enable();
+            DB::table('cats')->insert($ahuaData);
+            $cats = DB::table('cats')->select('name')->get();
+            $this->assertEquals(2, count($cats));
+            DB::table('cats')->where('name', '=', 'ahua')->delete();
+            for ($i = 0; $i < 10000; $i++) {
+                $cats = DB::table('cats')->select('name')->get();
+            }
+            $this->assertEquals(1, count($cats));
+            $xhprof_data = xhprof_disable();
+
+            $this->generateReport($xhprof_data, 'query-builder');
+            unlink($lockfile);
+        } else {
+            //------------------- Eloquent -------------------
+            xhprof_enable();
+            Cat::create($ahuaData);
+            $cats = Cat::all('name');
+            $this->assertEquals(2, count($cats));
+            Cat::where('name', '=', 'ahua')->delete();
+            for ($i = 0; $i < 10000; $i++) {
+                $cats = Cat::all('name');
+            }
+            $this->assertEquals(1, count($cats));
+            $xhprof_data = xhprof_disable();
+
+            $this->generateReport($xhprof_data, 'eloquent');
+            touch($lockfile);
+        }
+
+
+    }
+
 
 }
